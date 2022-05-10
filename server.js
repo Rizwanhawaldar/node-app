c00822e0e16b87562377a91dca0420dd391bbf18
Rizwanhawaldar <71437461+Rizwanhawaldar@users.noreply.github.com>
2022-05-10 15:31:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('<h1 style="color:green;">welcome to kubernets!!</h1> \n');
+  res.send('<h1 style="color:red;">welcome to kubernetes!!</h1> \n');
 });
 
 app.listen(PORT, HOST);
